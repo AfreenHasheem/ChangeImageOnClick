5f9d538f510266a01fad70088d4034698ce041b7
AfreenHasheem <afreen.hasheem@gmail.com>
2017-08-31 18:24:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/afreen/udemytoasts/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.afreen.udemytoasts;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public void onClicked(View view) {
+        ImageView image = (ImageView) findViewById(R.id.imageView1);
+        image.setImageResource(R.drawable.cat2);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
+
